02f088b34bf04d428445d88a1d0d66a64f37b9e1
Posei <firdausfajar19@gmail.com>
2021-10-12 23:09:11 +0700

    [State Machine] I forgor to add the actual state machine

---
+++ common/state_machine.gd
@@ -0,0 +1,43 @@
+extends Node
+class_name StateMachine
+
+var previous_state = null
+var states: Dictionary = {}
+var state: int = -1 setget set_state
+
+onready var parent = get_parent()
+
+
+func _physics_process(delta) -> void:
+	if state != -1:
+		_state_logic(delta)
+		var transition: int = _get_transition()
+		if transition != -1:
+			set_state(transition)
+
+
+func _state_logic(_delta) -> void:
+	pass
+
+
+func _get_transition() -> int:
+	return -1
+
+
+func _add_state(new_state: String) -> void:
+	states[new_state] = states.size()
+
+
+func set_state(new_state: int) -> void:
+	_exit_state(state)
+	previous_state = state
+	state = new_state
+	_enter_state(previous_state, state)
+
+
+func _enter_state(_previous_state: int, _new_state: int) -> void:
+	pass
+
+
+func _exit_state(_state_exited: int) -> void:
+	pass
