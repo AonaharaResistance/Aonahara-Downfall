1fc3cb67ef7c799c660065d6d3b27cbde3582c69
poseisharp <firdausfajar19@gmail.com>
2021-12-12 19:50:26 +0700

    [Forgor] i forgor so push just in case

--- entities/characters/emuwaa/skills/its_raining_rocks/its_raining_rocks.gd
+++ entities/characters/emuwaa/skills/its_raining_rocks/its_raining_rocks.gd
@@ -1,2 +1,14 @@
 extends Node2D
 
+var rocks = preload("res://entities/characters/emuwaa/skills/its_raining_rocks/rock_droplet.tscn")
+onready var sprite = $Sprite
+onready var sort = $YSort
+
+
+func _on_Timer_timeout():
+	var rock = rocks.instance()
+	sort.add_child(rock)
+	rock.global_position = (
+		sprite.global_position
+		+ Vector2(rand_range(-50, 50), rand_range(-50, 50))
+	)
