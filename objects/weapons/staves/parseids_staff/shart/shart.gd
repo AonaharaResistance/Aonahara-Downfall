72a755ad47ac93db98353ffb2af8ddcb69aa33cb
poseisharp <firdausfajar19@gmail.com>
2022-02-09 23:47:42 +0700

    [Emu] Final Shart Update

--- objects/weapons/staves/parseids_staff/shart/shart.gd
+++ objects/weapons/staves/parseids_staff/shart/shart.gd
@@ -2,8 +2,10 @@ extends Explosive
 
 export var speed: int = 200
 var direction = Vector2.ZERO
-onready var animation: AnimationPlayer = $ExplosionHitBox/AnimationPlayer
+onready var animation: AnimationPlayer = $AnimationPlayer
 onready var explosion_collision: CollisionShape2D = $ExplosionHitBox/CollisionShape2D
+onready var explosion: Area2D = $ExplosionHitBox
+var formed: bool = false
 
 
 func disable():
@@ -13,14 +15,23 @@ func disable():
 	explosion_collision.free()
 
 
-func _ready():
+func launch():
+	var angle_to_mouse = ((get_global_mouse_position() - Party.current_character().global_position).normalized()).angle()
+	rotate(angle_to_mouse)
 	var mouse_direction = (get_global_mouse_position() - global_position).normalized()
 	if mouse_direction.x < 0:
 		sprite.scale.y *= -1
+		explosion.scale.y *= -1
 
 
 func _process(delta):
-	global_position += speed * direction * delta
+	if formed:
+		global_position += speed * direction * delta
+
+
+func set_formed():
+	formed = true
+	animation.play("swim")
 
 
 func _on_Audio_finished():
